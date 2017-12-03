e2079f5d5703827f5d044ddbc7d21789cf230433
Ante Kvesic <kvesic_ante@yahoo.com>
2017-12-04 00:41:09 +0100

    add form for messages and list messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -7,10 +7,23 @@ socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 
-// socket.on('newMessage', function (message) {
-//     console.log('new message', message);
-// });
-
 socket.on('newMessage', function (message) {
     console.log(message.from + ": " + message.text);
+
+    var li = $("<li></li>");
+    li.text(`${message.from}: ${message.text}`);
+
+    $("#messages").append(li);
+});
+
+$('#message-form').on('submit', function(e) {
+    // prevent default behaviour of submit event, which includes refreshing the page
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('input[name="message"]').val()
+    }, function() {
+
+    });
 });
